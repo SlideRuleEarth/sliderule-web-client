64ee7600fca35fcfe6e5ac538c89928de7697b71
Carlos E. Ugarte <cugarteblair@gmail.com>
2024-01-11 15:04:24 -0500

    custom drawbox experiments

--- web-client/src/components/SrDrawButtonBox.vue
+++ web-client/src/components/SrDrawButtonBox.vue
@@ -1,12 +1,14 @@
 <template>
 <div>
     <SrRadioButton
+    class="sr-draw-button"
       v-model="picked"
       value="Polygon"
       icon="polyline"
     />
     <br>
     <SrRadioButton
+    class="sr-draw-button"
     v-model="picked"
     value="Box"
     icon="check_box_outline_blank"
@@ -21,3 +23,8 @@
   import SrRadioButton from './SrRadioButton.vue';
   const picked = ref('Polygon');
 </script>
+<style scoped>
+.sr-draw-button {
+    border: 1px solid #ccc; /* thin, solid, grey border */
+}
+</style>
--- web-client/src/components/SrRadioButton.vue
+++ web-client/src/components/SrRadioButton.vue
@@ -1,16 +1,19 @@
 <template>
-    <input
-      type="radio"
-      :value="value"
-      :checked="isChecked"
-      @change="$event.target && $emit('update:modelValue', ($event.target as HTMLInputElement).value)"
-    >
-    <i class="material-icons">{{ icon }}</i>
+    <label class="radio-button-label">
+      <input
+        type="radio"
+        :value="value"
+        :checked="isChecked"
+        @change="handleChange"
+        class="radio-input"
+      >
+      <i class="material-icons">{{ icon }}</i>
+    </label>
 </template>
   
 <script setup lang="ts">
   import { computed, defineProps } from 'vue';
-  
+
   const props = withDefaults(
     defineProps<{
       value?: string
@@ -24,14 +27,41 @@
     },
   );
   
-  defineEmits(['update:modelValue']);
-  
+  const emit = defineEmits(['update:modelValue']);
+
   const isChecked = computed(() => {
     return props.modelValue === props.value;
   });
+  const handleChange = (event: Event) => {
+    //console.log(event);
+    try{
+      const target = event.target as HTMLInputElement;
+      if (target) {
+        //console.log(target.value);
+        emit('update:modelValue', target.value);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 </script>
 <style scoped>
+    /* Visually hide the radio input but keep it accessible */
     input[type='radio'] {
         margin: 0.25rem;
+        opacity: 0;
+        width: 0;
+        height: 0;
+        position: absolute;
     }
-</style>
+  /* Style for the icon  */
+  .material-icons {
+    cursor: pointer;
+    font-weight:100
+  }
+  /* Style for the icon when the radio button is selected */
+  input[type="radio"]:checked + .material-icons {
+    /* Styles to highlight the active state, e.g., change color or add a border */
+    color: blue;
+    border: var(--border-width) solid var(--primary-color); 
+  }</style>
