d851275f0b3a38a9e361d18225392acfb8d6583d
Carlos E. Ugarte <cugarteblair@gmail.com>
2024-02-05 09:31:44 -0500

    WIP add WMS Nasa World view

--- web-client/src/composables/useWmsCap.ts
+++ web-client/src/composables/useWmsCap.ts
@@ -3,9 +3,11 @@ import ol_control_WMSCapabilities from 'ol-ext/control/WMSCapabilities';
 export function useWmsCap() {
     const wms_capabilities_cntrl= new ol_control_WMSCapabilities({
         srs: ['EPSG:2154'],
-        cors: true,
+        cors: false,
         optional: 'token',
         services: {
+            'Nasa GIBS': 'https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?',
+            'Nasa World View': 'https://worldview.earthdata.nasa.gov/wms',
             'Nasa WorldWind': 'https://worldwind25.arc.nasa.gov/wms',
             'USGS': 'https://www.opengis.net/wms',
         },
