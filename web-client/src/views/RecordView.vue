57d053c15008fca7f61018557208b3c726adf9e4
Carlos E. Ugarte <cugarteblair@gmail.com>
2024-03-22 09:17:08 -0400

    WIP components

--- web-client/src/views/RecordView.vue
+++ web-client/src/views/RecordView.vue
@@ -1,19 +1,16 @@
 <script setup>
-  import SrBuildDate from '@/components/SrBuildDate.vue';
 </script>
 
 <template>
   <div class="sr-about">
-    <p>PopularView placeholder</p>
+    <p>RecordView placeholder</p>
   </div>
 </template>
 
 <style scoped>
-@media (min-width: 1024px) {
   .sr-about {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
-}
 </style>
