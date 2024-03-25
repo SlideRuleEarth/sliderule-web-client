f8178bcc53dcd81c1a317c63e3df1f743d7fd5e5
Carlos E. Ugarte <cugarteblair@gmail.com>
2024-03-25 11:22:08 -0400

    WIP components

--- web-client/src/components/SrMenuMultiCheckInput.vue
+++ web-client/src/components/SrMenuMultiCheckInput.vue
@@ -1,100 +1,51 @@
 <template>
-    <div class="sr-menu-multi-check-input-wrapper">
-            <div ref="menuElement" class="sr-menu-multi-check-input-menu-control">
-                <label for="srSelectMultiCheckMenu-{{ label }}" class="sr-menu-multi-check-input-label">{{ label }}</label>
-                <SrCheckbox label="Select All" :default="true" @update:modelValue="handleSelectAllItems" />
-                <form class="sr-multi-check-menu-select-item" name="sr-select-multicheck-item-form">
-                    <select v-model="selectedMenuItems" class="sr-menu-multi-check-input-select-default" name="sr-select-multi-menu" id="srSelectMultiMenu-{{ label }}" multiple>
-                        <option v-for="item in menuOptions" :value="item" :key="item">
-                            {{ item }}
-                            <SrCheckbox label="Select" :default="true" />
-                        </option>
-                    </select>
-                </form>
-            </div>
+  <div class="multi-select-container">
+    <div v-for="option in options" :key="option.value">
+      <input
+        type="checkbox"
+        :value="option.value"
+        v-model="selectedValues"
+      />
+      <label>{{ option.label }}</label>
+
+      <input 
+        type="checkbox" 
+        v-model="option.additionalParameter" 
+        :disabled="!option.selected"
+        @change="handleAdditionalParamChange(option)" 
+      /> 
+      <label>Additional Parameter</label>
     </div>
+  </div>
 </template>
-  
+
 <script setup lang="ts">
-    import { ref, onMounted } from 'vue';
-    import SrCheckbox from './SrCheckbox.vue';
+import { ref, watch } from 'vue';
 
-    const props = defineProps({
-        label: String,
-        menuOptions: Array as () => string[],
-        default: Array as () => string[]
-    });
+interface Option {
+  label: string;
+  value: string; // Or other relevant type
+  selected: boolean;
+  additionalParameter: boolean;
+}
 
-    const selectAll = ref(true);
+const options = ref<Option[]>([
+  { label: 'Option 1', value: 'opt1', selected: false, additionalParameter: false },
+  { label: 'Option 2', value: 'opt2', selected: false, additionalParameter: false },
+  { label: 'Option 3', value: 'opt3', selected: false, additionalParameter: false },
+]);
 
-    const handleSelectAllItems = (event:any) => {
-        console.log('CheckSelectAll:', event);
-        selectAll.value = event;
-        if(props.menuOptions){
-            if(selectAll.value){
-                console.log('Select All:', props.menuOptions);
-                selectedMenuItems.value = props.menuOptions.map(item => item);
-            } else {
-                console.log('Deselect All:', props.menuOptions);
-                selectedMenuItems.value = [];
-            }
-        } else {
-            console.error('No menu options to select?');
-            selectedMenuItems.value = [];
-        }
-        console.log('Selected Items:', selectedMenuItems.value);
-    };
-    // Update to manage an array of selected items
-    const selectedMenuItems = ref(props.default);
+const selectedValues = ref<string[]>([]); 
 
-    onMounted(() => {
-        console.log('Mounted Menu:', props.label);
-    });
+function handleAdditionalParamChange(option: Option) {
+  // You can add any extra logic for managing the additional parameter here 
+  // Example: Send updated option data to an API 
+}
+
+// Watch for changes in selectedValues and update the options accordingly
+watch(selectedValues, (newSelectedValues: string[]) => {
+  options.value.forEach(option => {
+    option.selected = newSelectedValues.includes(option.value);
+  });
+});
 </script>
-
-<style scoped>
-.sr-menu-multi-check-input-wrapper {
-    border: 1px solid transparent;
-    border-top: 0.125rem solid transparent;
-    border-radius: var(--border-radius);
-    margin-top: 0.125rem;
-}
-
-.sr-menu-multi-check-input-label {
-    margin-right: 0.5rem;
-}
-
-.sr-menu-multi-check-input-select-item {
-    display: flex;
-    align-items: right;
-}
-
-.sr-menu-multi-check-input-menu-control {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    gap: 10px;
-}
-
-.sr-menu-multi-check-input-select-default {
-    width: calc(100% - 0.25rem);
-    padding: 0rem;
-    margin: 0rem;
-    color: white;
-    background-color: black;
-    border-radius: var(--border-radius);
-    height: auto; /* Adjust height to fit multiple selections */
-    overflow-y: auto; /* Allows scrolling through options */
-}
-
-.sr-multi-check-menu-select-item {
-    width: 100%;
-    min-width: 10rem;
-    height: auto;
-    overflow-y: auto;
-    padding: 0rem;
-    margin: 0rem;
-}
-
-</style>
--- web-client/src/uploadDate.js
+++ web-client/src/uploadDate.js
@@ -1,1 +1,1 @@
-export const UPLOAD_DATE = '2024-03-25 10:11:45';
+export const UPLOAD_DATE = '2024-03-25 11:16:13';
