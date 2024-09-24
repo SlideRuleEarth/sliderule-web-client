ac9831911c4424bcfbe57fbd8cd0cc72402f7681
Carlos E. Ugarte <cugarteblair@gmail.com>
2024-09-24 12:35:03 -0400

    terraform cleanup

--- terraform/modules/bucket.tf
+++ terraform/modules/bucket.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "my_site_bucket" {
+resource "aws_s3_bucket" "webclient_site_bucket" {
   bucket = var.domainName
   force_destroy = true
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.my_site_bucket.id
+resource "aws_s3_bucket_public_access_block" "web_client_site_access_block" {
+  bucket = aws_s3_bucket.webclient_site_bucket.id
 
   block_public_acls         = true
   block_public_policy       = true
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_public_access_block" "example" {
 data "aws_iam_policy_document" "s3_policy" {
   statement {
     actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.my_site_bucket.arn}/*"]
+    resources = ["${aws_s3_bucket.webclient_site_bucket.arn}/*"]
 
     principals {
       type        = "AWS"
@@ -29,6 +29,6 @@ data "aws_caller_identity" "current" {
 
 
 resource "aws_s3_bucket_policy" "web" {
-  bucket = aws_s3_bucket.my_site_bucket.id
+  bucket = aws_s3_bucket.webclient_site_bucket.id
   policy = data.aws_iam_policy_document.s3_policy.json
 }
