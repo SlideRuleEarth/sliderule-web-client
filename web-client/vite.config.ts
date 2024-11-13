4689371da7c418bf8c2d2438dc92d0f74d4d9b18
Carlos E. Ugarte <cugarteblair@gmail.com>
2024-11-13 08:39:23 -0500

    make preview run production bundle locally

--- web-client/vite.config.ts
+++ web-client/vite.config.ts
@@ -1,13 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueDevTools(),
+    createHtmlPlugin({})
   ],
+  server: {
+    hmr: {
+      overlay: false // disable overlay to reduce rendering load on reloads
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -15,5 +24,8 @@ export default defineConfig({
   },
   build: {
     sourcemap: process.env.VITE_BUILD_SOURCEMAP === 'true' // conditionally Enable source maps for production build
+  },
+  optimizeDeps: {
+    include: ['@duckdb/duckdb-wasm'] // Explicitly pre-bundle any large dependencies you suspect might be slow
   }
 })
