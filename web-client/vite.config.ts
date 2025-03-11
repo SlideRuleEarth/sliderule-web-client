252ba0ab1785ac21be238d59bc10afdb9b762d6f
Carlos E. Ugarte <cugarteblair@gmail.com>
2025-03-11 15:31:58 -0400

    experiment with vite bundle options

--- web-client/vite.config.ts
+++ web-client/vite.config.ts
@@ -23,9 +23,28 @@ export default defineConfig({
     }
   },
   build: {
-    sourcemap: process.env.VITE_BUILD_SOURCEMAP === 'true' // conditionally Enable source maps for production build
-  },
-  optimizeDeps: {
-    include: ['@duckdb/duckdb-wasm'] // Explicitly pre-bundle any large dependencies you suspect might be slow
-  }
+    sourcemap: process.env.VITE_BUILD_SOURCEMAP === 'true', // conditionally enable source maps for production build
+    rollupOptions: {
+		output: {
+			manualChunks(id) {
+			// Ensure @luma.gl/webgl and @deck.gl/core are in the same chunk
+			if (id.includes('@luma.gl/webgl') || id.includes('@deck.gl/core')) {
+				return 'deckgl-bundle';
+			}
+			// Group large dependencies together
+			if (id.includes('@duckdb/duckdb-wasm')) {
+				return 'duckdb-bundle';
+			}
+			// Separate node_modules into a common vendor chunk
+			if (id.includes('node_modules')) {
+				return 'vendor';
+			}
+			return null;
+			}
+		}
+		}
+	},
+  	optimizeDeps: {
+    	include: ['@duckdb/duckdb-wasm'] // Explicitly pre-bundle large dependencies
+  	}
 })
