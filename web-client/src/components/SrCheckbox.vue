cede3cc005e343eb63d9cc7fe3d7f64f8241e09a
Carlos E. Ugarte <cugarteblair@gmail.com>
2025-05-02 11:56:05 -0400

    Style changes in v1.15.0 broke the SrCheckbox #470

--- web-client/src/components/SrCheckbox.vue
+++ web-client/src/components/SrCheckbox.vue
@@ -1,110 +1,95 @@
 <template>
-    <div class="sr-checkbox">
-      <template v-if="labelOnRight">
-        <input 
-          :id="'sr-checkbox-' + label" 
-          type="checkbox" 
-          v-model="localChecked" 
-          :disabled="insensitive"
-          @change="emitChange"
-        />
-      </template>
-      <template v-if="tooltipText || tooltipUrl">
-        <SrLabelInfoIconButton 
-          :label="label" 
-          :tooltipText="tooltipText" 
-          :tooltipUrl="tooltipUrl" 
-          :insensitive="insensitive" 
-          :labelFontSize="labelFontSize"
-        />
-      </template>
-      <template v-else>
-        <label 
-          :for="'sr-checkbox-' + label" 
-          :class="['sr-checkbox-label', { 'sr-checkbox-label-insensitive': insensitive }]"
-          :style="{ fontSize: labelFontSize }"
-        >
-          {{ label }}
-        </label>
-      </template>
-      <template v-if="!labelOnRight">
-        <input 
-          :id="'sr-checkbox-' + label" 
-          type="checkbox" 
-          v-model="localChecked" 
-          :disabled="insensitive"
-          @change="emitChange"
-        />
-      </template>
-    </div>
-  </template>
+  <div class="sr-checkbox">
+    <Checkbox 
+      :inputId="'sr-checkbox-' + label" 
+      v-model="localChecked" 
+      :binary="true" 
+      :disabled="insensitive"
+      @change="emitChange"
+      size="small"
+    />
+
+    <template v-if="tooltipText || tooltipUrl">
+      <SrLabelInfoIconButton 
+        :label="label" 
+        :tooltipText="tooltipText" 
+        :tooltipUrl="tooltipUrl" 
+        :insensitive="insensitive" 
+        :labelFontSize="labelFontSize"
+      />
+    </template>
+    <template v-else>
+      <label 
+        :for="'sr-checkbox-' + label" 
+        :class="['sr-checkbox-label', { 'sr-checkbox-label-insensitive': insensitive }]"
+        :style="{ fontSize: labelFontSize }"
+      >
+        {{ label }}
+      </label>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch, onMounted, isRef, WritableComputedRef } from 'vue';
+import Checkbox from 'primevue/checkbox';
+import SrLabelInfoIconButton from './SrLabelInfoIconButton.vue';
+
+const props = withDefaults(
+  defineProps<{
+    label?: string;
+    modelValue: boolean | WritableComputedRef<boolean>;
+    insensitive?: boolean;
+    tooltipText?: string;
+    tooltipUrl?: string;
+    labelFontSize?: string;
+    defaultValue?: boolean;
+  }>(),
+  {
+    label: '',
+    modelValue: false,
+    insensitive: false,
+    tooltipText: '',
+    tooltipUrl: '',
+    labelFontSize: 'small',
+    defaultValue: false,
+  }
+);
+
+const emit = defineEmits(['update:modelValue']);
+
+const getValue = (value: boolean | WritableComputedRef<boolean>) =>
+  isRef(value) ? value.value : value;
+
+const setValue = (value: boolean | WritableComputedRef<boolean>, newValue: boolean) => {
+  if (isRef(value)) {
+    value.value = newValue;
+  } else {
+    emit('update:modelValue', newValue);
+  }
+};
+
+const localChecked = ref(getValue(props.modelValue));
+
+watch(() => props.modelValue, (newValue) => {
+  localChecked.value = getValue(newValue);
+});
+
+watch(localChecked, (newValue) => {
+  setValue(props.modelValue, newValue);
+});
+
+onMounted(() => {
+  setValue(props.modelValue, props.defaultValue);
+});
+
+const emitChange = () => {
+  // Optional logging
+};
+</script>
+
   
-  <script setup lang="ts">
-  import { ref, watch, onMounted, isRef, WritableComputedRef } from 'vue';
-  import SrLabelInfoIconButton from './SrLabelInfoIconButton.vue';
-  
-  // Props definition
-  const props = withDefaults(
-    defineProps<{
-      label?: string;
-      modelValue: boolean | WritableComputedRef<boolean>;
-      insensitive?: boolean;
-      tooltipText?: string;
-      tooltipUrl?: string;
-      labelFontSize?: string;
-      labelOnRight?: boolean;
-      defaultValue?: boolean;
-    }>(),
-    {
-      label: '',
-      modelValue: false,
-      insensitive: false,
-      tooltipText: '',
-      tooltipUrl: '',
-      labelFontSize: 'small',
-      labelOnRight: false,
-      defaultValue: false,
-    }
-  );
-  
-  const emit = defineEmits(['update:modelValue']);
-  
-  // Helper function to get or set ref values
-  const getValue = (value: boolean | WritableComputedRef<boolean>) =>
-    isRef(value) ? value.value : value;
-  
-  const setValue = (value: boolean | WritableComputedRef<boolean>, newValue: boolean) => {
-    if (isRef(value)) {
-      value.value = newValue;
-    } else {
-      emit('update:modelValue', newValue);
-    }
-  };
-  
-  // Local state
-  const localChecked = ref(getValue(props.modelValue));
-  
-  // Sync `modelValue` with `localChecked`
-  watch(() => props.modelValue, (newValue) => {
-    localChecked.value = getValue(newValue);
-  });
-  
-  // Emit changes to `modelValue` when `localChecked` changes
-  watch(localChecked, (newValue) => {
-    setValue(props.modelValue, newValue);
-  });
-  
-  // Initialize `localChecked` on mount
-  onMounted(() => {
-    setValue(props.modelValue, props.defaultValue);
-  });
-  
-  const emitChange = () => {
-    //console.log(`SrCheckbox: ${props.label}: ${localChecked.value}`);
-  };
-  </script>
-  
-  <style scoped>
+<style scoped>
   .sr-checkbox {
     display: flex;
     align-items: center;
@@ -121,5 +106,5 @@
     white-space: nowrap;
     color: #888; /* grey color */
   }
-  </style>
+</style>
   
