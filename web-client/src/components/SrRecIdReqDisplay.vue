9730137d0ac801584cfc2370f22faa57684ef9fc
Carlos E. Ugarte <cugarteblair@gmail.com>
2025-12-18 11:37:40 -0500

    Handle python code when importing or pasting a request into the request edit view and exporting for python client Fixes #871

--- web-client/src/components/SrRecIdReqDisplay.vue
+++ web-client/src/components/SrRecIdReqDisplay.vue
@@ -1,102 +1,98 @@
 <template>
-    <div class="sr-rec-req-display-panel">
-        <Button 
-            icon="pi pi-eye" 
-            :label="props.label"
-            class="sr-glow-button"
-            @click="openParmsDialog(reqParms)"
-            @mouseover="tooltipRef?.showTooltip($event, props.tooltipText)"
-            @mouseleave="tooltipRef?.hideTooltip"
-            variant="text"
-            rounded
-            :disabled="props.insensitive"
-        ></Button>
-        <SrCustomTooltip ref="tooltipRef" id="recIdDisplayTooltip"/>
-        <SrJsonDisplayDialog
-            v-model:visible="showParmsDialog"
-            :json-data="reqParms"
-            :title="`endpoint = ${curAPI}`"
-            width="50vw"
-        />
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-    import { ref,onMounted } from "vue";
-    import Button from "primevue/button";
-    import { db } from "@/db/SlideRuleDb";
-    import SrJsonDisplayDialog from "./SrJsonDisplayDialog.vue";
-    import SrCustomTooltip from "./SrCustomTooltip.vue";
-    import { createLogger } from '@/utils/logger';
+  <div class="sr-rec-req-display-panel">
+    <Button
+      icon="pi pi-eye"
+      :label="props.label"
+      class="sr-glow-button"
+      @click="openParmsDialog(reqParms)"
+      @mouseover="tooltipRef?.showTooltip($event, props.tooltipText)"
+      @mouseleave="tooltipRef?.hideTooltip"
+      variant="text"
+      rounded
+      :disabled="props.insensitive"
+    ></Button>
+    <SrCustomTooltip ref="tooltipRef" id="recIdDisplayTooltip" />
+    <SrReqParmsDisplayDlg
+      v-model:visible="showParmsDialog"
+      :json-data="reqParms"
+      :title="`endpoint = ${curAPI}`"
+      :endpoint="curAPI"
+      width="80vw"
+    />
+  </div>
+</template>
 
-    const logger = createLogger('SrRecIdReqDisplay');
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import Button from 'primevue/button'
+import { db } from '@/db/SlideRuleDb'
+import SrReqParmsDisplayDlg from './SrReqParmsDisplayDlg.vue'
+import SrCustomTooltip from './SrCustomTooltip.vue'
+import { createLogger } from '@/utils/logger'
 
-    const props = withDefaults(
-        defineProps<{
-            reqId: number;
-            label: string;
-            insensitive?: boolean;
-            tooltipText?: string;
-        }>(),
-        {
-            reqId: 0,
-            label: 'Show Req Parameters',
-            insensitive: false,
-            tooltipText: 'click to show request parameters for current record',
-            tooltipUrl: '',
-            labelFontSize: 'small',
-            labelOnRight: false
-        }
-    );
+const logger = createLogger('SrRecIdReqDisplay')
 
-    const showParmsDialog = ref(false);
-    const reqParms = ref<string>('');
-    const curAPI = ref<string>('');
-    const tooltipRef = ref();
-
-    onMounted(async () => {
-        if(props.reqId) {
-            curAPI.value = await db.getFunc(props.reqId);
-            const p = await db.getReqParams(props.reqId);
-            reqParms.value = JSON.stringify(p, null, 2);
-        } else {
-            logger.debug('onMounted: no reqId');
-        }
-    });
-    // Open the Parms dialog
-    function openParmsDialog(params: string | object) {
-        if (typeof params === 'object') {
-            reqParms.value = JSON.stringify(params, null, 2);
-        } else {
-            reqParms.value = params;
-        }
-        showParmsDialog.value = true;
-    }
-
-  </script>
-  
-  <style scoped>
-  /* Style your button and component here */
-  .sr-rec-req-display-panel {
+const props = withDefaults(
+  defineProps<{
+    reqId: number
+    label: string
+    insensitive?: boolean
+    tooltipText?: string
+  }>(),
+  {
+    reqId: 0,
+    label: 'Show Req Parameters',
+    insensitive: false,
+    tooltipText: 'click to show request parameters for current record',
+    tooltipUrl: '',
+    labelFontSize: 'small',
+    labelOnRight: false
   }
+)
 
-.sr-rec-req-display-parms {
-    position: relative;
-    margin-top: 0rem;
-    display: flex;
-    justify-content: column;
-    max-height: 15rem;
-    max-width: 15rem;
-    min-height: 10rem;
-    overflow-y: auto;
-    overflow-x: auto;
-    width: 100%;
-    text-overflow: clip;
-    scrollbar-width: none; /* Firefox */
-    -ms-overflow-style: none; /* Internet Explorer 10+ */
+const showParmsDialog = ref(false)
+const reqParms = ref<string>('')
+const curAPI = ref<string>('')
+const tooltipRef = ref()
+
+onMounted(async () => {
+  if (props.reqId) {
+    curAPI.value = await db.getFunc(props.reqId)
+    const p = await db.getReqParams(props.reqId)
+    reqParms.value = JSON.stringify(p, null, 2)
+  } else {
+    logger.debug('onMounted: no reqId')
+  }
+})
+// Open the Parms dialog
+function openParmsDialog(params: string | object) {
+  if (typeof params === 'object') {
+    reqParms.value = JSON.stringify(params, null, 2)
+  } else {
+    reqParms.value = params
+  }
+  showParmsDialog.value = true
+}
+</script>
+
+<style scoped>
+/* Style your button and component here */
+.sr-rec-req-display-panel {
 }
 
-
-
+.sr-rec-req-display-parms {
+  position: relative;
+  margin-top: 0rem;
+  display: flex;
+  justify-content: column;
+  max-height: 15rem;
+  max-width: 15rem;
+  min-height: 10rem;
+  overflow-y: auto;
+  overflow-x: auto;
+  width: 100%;
+  text-overflow: clip;
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none; /* Internet Explorer 10+ */
+}
 </style>
-  
